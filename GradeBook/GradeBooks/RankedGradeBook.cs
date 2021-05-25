6afcdb2c0d68a8f8325808e3b726424b3f1919a7
luisairam <luisairam@outlook.com>
2021-05-25 16:23:29 -0500

    Average

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -9,6 +10,29 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count > 5)
+            {
+                throw new InvalidOperationException();
+
+            }
+
+            Students.OrderBy(x => x.AverageGrade);
+            int total = Students.Count;
+            int pos = total - Students.SkipWhile(x => x.AverageGrade == averageGrade).Count();
+            double porc = (pos * 100) / total;
+
+            if (porc <= 20)
+                return 'A';
+            if (porc > 20 && porc <= 40)
+                return 'B';
+            if (porc > 40 && porc <= 60)
+                return 'C';
+            if (porc > 60 && porc <= 80)
+                return 'D';
+            return 'F';
         }
     }
 }
